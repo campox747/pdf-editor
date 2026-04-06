d4204455587e3634d6c0d7d4b85970325c4b13ec
Francesco <francescocamporeale23@gmail.com>
2026-04-06 19:14:35 +0200

    feature: further development of the PyQt6 UI for PDFs merging

--- core/gui_merge.py
+++ core/gui_merge.py
@@ -1,19 +1,67 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
-# Define the window to be displayed
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+    QFileDialog
+)
+
+# Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
+        self.setWindowTitle("Merge Files")
 
-        button = QPushButton("Press Me!")
-        button.setCheckable(True)
-        button.clicked.connect(self.the_button_was_clicked)
+        # Create the label
+        label = QLabel("Please select the files you want to merge in order:")
+        font = label.font()
+        font.setPointSize(15)
+        label.setFont(font)
+        label.setAlignment(Qt.AlignmentFlag.AlignLeft)
 
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        file1 = QLabel("Your Files:")
+        files_button = QPushButton("Browse Files...")
 
-    def the_button_was_clicked(self):
-        print("Clicked!")
+        files_button.clicked.connect(self.open_file_browser)
+
+        # Create a layout to hold your items
+        layout = QVBoxLayout()
+        layout.setContentsMargins(30, 15, 30, 15)
+
+        layout.addWidget(label)
+
+        layout.addSpacing(30)
+
+        layout.addWidget(file1)
+        layout.addWidget(files_button, alignment=Qt.AlignmentFlag.AlignLeft)
+
+        layout.addStretch()
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+    # Open file browser and make user selct files to merge
+    def open_file_browser(self):
+        file_paths, _ = QFileDialog.getOpenFileNames(
+            self,
+            "Select PDFs to Merge",    
+            "",                        
+            "PDF Files (*.pdf)"        
+        )
+
+        if file_paths:
+            print(f"Files: {file_paths}")
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
